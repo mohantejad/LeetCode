316dad0c0200f4deef1ae7cf7d7e8bcf4d7fe821
Mohanteja Dharmavarapu <mt.dharmavarapu@gmail.com>
2026-02-28 14:26:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,13 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i, j, k = m - 1, n - 1, m + n - 1
-        
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
+        # Bruteforce - O(n + (m+n)log(m+n))
+        nums1[m:] = nums2
+        nums1.sort()
